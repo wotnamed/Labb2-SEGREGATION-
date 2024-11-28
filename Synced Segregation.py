c13e21f830b0d785d3e223cbb72059c4606fcde8
wotnamed <sjogrenalvin@icloud.com>
2024-11-28 14:26:44 +0100

    luas är en beta

--- Synced Segregation.py
+++ Synced Segregation.py
@@ -5,19 +5,19 @@ import math
 
 
 class Actor:
-    def __init__(self, color):
-        self.color = color
-        self.is_satisfied = False
+    def __init__(self, color):  # upon initialisation
+        self.color = color  # class colour (determines what other actors it will be "satisfied" with.
+        self.is_satisfied = False   # the attribute that determines whether the actor will be attempted to move or not
 
 
-class NeighboursApp:
-    def __init__(self, root):
+class NeighboursApp: # the container for the program (most variables and functions are declared here)
+    def __init__(self, root):   # upon initialising an instance
         # Canvas dimensions and margin
-        self.width = 500
-        self.height = 500
-        self.margin = 50
-        self.dot_size = 0
-        self.interval = 0.5  # in seconds
+        self.width = 500  # window width
+        self.height = 500  # window height
+        self.margin = 50  # margin for window width and height
+        self.dot_size = 0  # This doesn't do shit (can be removed)
+        self.interval = 0.5  # The interval used between each operation in seconds (ie 2 means a delay of 2 seconds)
 
         # Initialize tkinter canvas
         self.root = root
@@ -25,7 +25,7 @@ class NeighboursApp:
         self.canvas.pack()
 
         # Initialize world
-        self.world = []
+        self.world = []  # create first list for world (will be matrix later)
         self.init_world()
         self.last_update_time = time.time()
 
@@ -36,27 +36,23 @@ class NeighboursApp:
         # init an empty world
 
     def init_world(self):
-        # Distribution percentages for RED, BLUE, and NONE
-        dist = [0.25, 0.25, 0.5]
-        possible = ["Red", "Blue", None]
-        n_locations = 25000 # Number of locations (should be a square and also work for 25000)
-        y = int(n_locations**0.5)
-        x = int(n_locations**0.5)
+        dist = [0.25, 0.25, 0.5]  # the distribution of each colour declared below (the weights used in random.choices)
+        possible = ["Red", "Blue", None]  # The possible colours for an agent. None means blank space.
+        n_locations = 25000  # This determines the numbers of "locations" with in the matrix. It should be a product of a^2 where a is a whole number and not below zero.
+        y = int(n_locations**0.5)  # y height equals sqrt number of locations
+        x = int(n_locations**0.5)  # x width equals sqrt number of locations (the grid becomes a square)
         for i in range(y):
             self.world.append([])
         for i in self.world:
-            for i in range(x):
-                value = random.choices(possible, dist)[0]
-                if value is not None:
-                    self.world[i].append(Actor(value))
-                else:
-                    self.world[i].append(None)
-
-
+            for i in range(x):  # for every row in matrix
+                value = random.choices(possible, dist)[0]  # Determines whether the item that is about to be assigned is an actor or not and what colour that actor obtains.
+                if value is not None:  # If the item is an actor
+                    self.world[i].append(Actor(value))  # Append the actor to the matrix
+                else:  # If the item is not an actor
+                    self.world[i].append(None)  # Append None
         # coordinates: list[y][x]
 
-        # Adjust screen size based on the number of locations
-        self.fix_screen_size(n_locations)
+        self.fix_screen_size(n_locations) # Adjust screen size based on the number of locations
 
     def test_neighbours(self, row_number, column_number):
         subject = self.world[row_number][column_number].color
@@ -81,7 +77,8 @@ class NeighboursApp:
                         bad_neighbours += 1
 
         return 1-(bad_neighbours/total_neighbours)
-    def find_unhappy_actors(self, threshold, unhappy):
+
+    def find_unhappy_actors(self, threshold, unhappy):  # Find all unhappy actors and append them to the "unhappy" list.
         for row in range(len(self.world)):
             for col in range(len(self.world[row])):
                 if self.world[row][col] is None:
@@ -91,8 +88,8 @@ class NeighboursApp:
                     unhappy.append((row,col))
 
     def remove_and_redistribute_actors(self, unhappy, size):
-        for row, col in unhappy:
-            actor = self.world[row][col]
+        for row, col in unhappy:  # For every unhappy actor in the list
+            actor = self.world[row][col]  # Bookmark the unhappy actor
             self.world[row][col] = None  # Remove actor from current location
             # Find a new empty location
             while True:
@@ -100,7 +97,7 @@ class NeighboursApp:
                 new_col = random.randint(0, size - 1)
                 if self.world[new_row][new_col] is None:  # Found an empty spot
                     self.world[new_row][new_col] = actor
-                    # print(f'({row}, {col}) with satisfaction {satisfaction} moved to ({new_row},{new_col})')
+                    # print(f'({row}, {col}) with satisfaction {satisfaction} moved to ({new_row},{new_col})')  # No longer used
                     break
 
 
@@ -108,18 +105,14 @@ class NeighboursApp:
         threshold = 0.8
         size = len(self.world)
         unhappy = []
-        satisfaction = 0
         self.find_unhappy_actors(threshold, unhappy)
         self.remove_and_redistribute_actors(unhappy, size)
-
-
-
 
 
     def is_valid_location(self, size, row, col):
         return 0 <= row < size and 0 <= col < size
 
-
+    
 
 # ============================ YOU DON'T HAVE TO CARE ABOUT THE CODE BELOW IN THE ORIGINAL VERSION, ONLY USED FOR DRAWING ============================
     def fix_screen_size(self, n_locations):
